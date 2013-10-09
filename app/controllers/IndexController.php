cef21820563053d6f94d8150130b65cfc71b9de2
Jota Junior <jotavrj@gmail.com>
2013-10-09 03:27:17 -0300

    Echo rendered view

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -10,14 +10,16 @@ class IndexController extends \Phalcon\Mvc\Controller
 	public function indexAction()
 	{
 		$auth = $this->users->facebookAuth();
-
+									echo 2;
 		if (!$auth) {
 			$facebook_login_url = $this->users->getFacebookLoginUrl();
 			
 			echo $this->view->render('landing/index', array(
-														   "facebook_login_url" => $facebook_login_url
-														  )
-									);
+													   "facebook_login_url" => $facebook_login_url
+													  )
+								);
+							echo 1;
+
 		} else {
 			echo "Ola, ".$this->session->get('name');
 						echo "<a href='/liivro/index/logout'>logout</a>";
