899911fcb3629d76f0bf478c74e0c4ef0e4b17b6
Jota Junior <jotavrj@gmail.com>
2013-10-09 03:19:58 -0300

    New layout for landing page.

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -12,12 +12,17 @@ class IndexController extends \Phalcon\Mvc\Controller
 		$auth = $this->users->facebookAuth();
 		
 		if (!$auth) {
-			echo "<a href='".$this->users->getFacebookLoginUrl()."'>Login</a>|||";
+			$facebook_login_url = $this->users->getFacebookLoginUrl();
+			
+			$this->view->render('landing/index', array(
+													   "facebook_login_url" => $facebook_login_url
+													  )
+								);
 
 		} else {
 			echo "Ola, ".$this->session->get('name');
+						echo "<a href='/liivro/index/logout'>logout</a>";
 		}
-					echo "<a href='/liivro/index/logout'>logout</a>";
 	}
 	
 	public function logoutAction()
---
+++ app/views/landing/index.php
@@ -0,0 +1,359 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"><![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"><![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"><!--<![endif]-->
+
+<head>
+    <title>Liivro</title>
+    
+    <meta name="viewport" content="width=100%; initial-scale=1; maximum-scale=1; minimum-scale=1; user-scalable=no;" />
+    <link rel="shortcut icon" href="images/favicon.ico" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/apple-touch-icon-144-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/apple-touch-icon-114-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/apple-touch-icon-72-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-57-precomposed.png" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/prettyPhoto.css" />
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+    <!--[if IE 7]>
+    <link rel="stylesheet" type="text/css" href="css/font-awesome-ie7.min.css"/>
+    <![endif]-->
+
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="js/jquery.quicksand.js"></script>
+    <script type="text/javascript" src="js/superfish.js"></script>
+    <script type="text/javascript" src="js/hoverIntent.js"></script>
+    <script type="text/javascript" src="js/jquery.flexslider.js"></script>
+    <script type="text/javascript" src="js/jflickrfeed.min.js"></script>
+    <script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+    <script type="text/javascript" src="js/jquery.elastislide.js"></script>
+    <script type="text/javascript" src="js/jquery.tweet.js"></script>
+    <script type="text/javascript" src="js/smoothscroll.js"></script>
+    <script type="text/javascript" src="js/jquery.ui.totop.js"></script>
+    <script type="text/javascript" src="js/ajax-mail.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+    <!--[if lt IE 9]>
+    <script type="text/javascript" src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <link rel="stylesheet" type="text/css" href="css/layerslider.css" />
+    <script type="text/javascript" src="js/layerslider.kreaturamedia.jquery.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+
+<body>
+
+<!-- start: Top Menu -->
+<div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <a class="brand" href="#"><i class="icon-cloud "></i> Liivro</a>
+            <div class="nav-collapse collapse">
+                <ul class="nav pull-right">
+                    <li class="active"><a href="./index.html">Home</a></li>
+                    <li><a href="./contact.html">Contato</a></li>
+                	<li><a href="./contact.html">Blog</a></li>
+                	<li><a href="<?=$facebook_login_url;?>"><strong>Entrar com Facebook</strong></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- start: Top Menu -->
+
+<!-- start: Container -->
+<div class="container">
+
+    <div class="row">
+
+        <!-- start: Page section -->
+        <section id="page-sidebar" class="span12">
+
+        <div class="page-inner">
+
+        <div class="row-fluid">
+            <!-- start: Slider -->
+            <section id="slider" class="hidden-phone">
+
+                <div id="layerslider-container">
+
+                    <div id="layerslider" style="width: 960px; height: 330px;">
+
+
+                        <div class="ls-layer slide1">
+
+                            <img src="example/layerslider/slider-a2.jpg" class="ls-bg" />
+                            <!--<img src="example/layerslider/tn_1_2.jpg" class="ls-tn">-->
+
+                            <img class="ls-s2" src="example/layerslider/l14.png" style="
+                                         top: 10px;
+                                         left: 470px;
+                                         delayin: 0;
+                                         durationin: 10000;
+                                         slidedirection: left;
+                                         easingin: easeOutQuart;
+                                         delayout: 0;
+                                         durationout: 3000;
+                                         slideoutdirection: right;
+                                         easingout: easeInOutQuint;
+                                        " />
+
+                            <h2 class="ls-s4 ls-head" style="
+                                        top: 70px;
+                                        left: 70px;
+                                        delayin : 400;
+                                        durationin : 3000;
+                                        slidedirection: right;
+                                        easingin : easeOutQuart;
+                                        delayout: 0;
+                                        durationout : 3000;
+                                        slideoutdirection: left;
+                                        easingout : easeInOutQuint;
+                                        ">UFMG</h2>
+                            <h3 class="ls-s4 ls-label" style="
+                                        top: 120px;
+                                        left: 70px;
+                                        delayin: 800;
+                                        durationin : 3000;
+                                        slidedirection: right;
+                                        easingin : easeOutQuart;
+                                        delayout: 0;
+                                        durationout : 3000;
+                                        slideoutdirection: left;
+                                        easingout : easeInOutQuint;
+                                        ">Troque e venda livros didáticos</h3>
+                            <p class="ls-s4 ls-sublabel" style="
+                                        top: 165px;
+                                        left: 70px;
+                                        delayin: 1000;
+                                        durationin : 3000;
+                                        slidedirection: right;
+                                        easingin : easeOutQuart;
+                                        delayout: 0;
+                                        durationout : 3000;
+                                        slideoutdirection: left;
+                                        easingout : easeInOutQuint;
+                                        ">Entre com sua conta do Facebook!</p>
+                            <p class="ls-s4 ls-sublabel" />
+
+
+                        </div>
+
+
+                        <div class="ls-layer slide2">
+
+                            <img src="example/layerslider/slider-a1.jpg" class="ls-bg" />
+                            <!--<img src="example/layerslider/tn_1_1.jpg" class="ls-tn">-->
+
+                            <img class="ls-s1" src="example/layerslider/cl-1.png" style="
+                                     top: 20px;
+                                     left: 50px;
+                                     delayin: 0;
+                                     durationin: 12000;
+                                     slidedirection: right;
+                                     easingin: easeOutQuart;
+                                     delayout: 0;
+                                     durationout: 1500;
+                                     slideoutdirection: left;
+                                     easingout: easeInOutQuint;
+                                     " />
+                            <img class="ls-s2" src="example/layerslider/l13.png" style="
+                                     top: 20px;
+                                     left: 570px;
+                                     delayin: 0;
+                                     durationin: 5000;
+                                     slidedirection: left;
+                                     easingin: easeOutQuart;
+                                     delayout: 0;
+                                     durationout: 1500;
+                                     slideoutdirection: right;
+                                     easingout: easeInOutQuint;
+                                    " />
+                            <img class="ls-s3" src="example/layerslider/cl-2.png" style="
+                                     top: 0px;
+                                     left: -250px;
+                                     delayin : 0;
+                                     durationin : 10000;
+                                     slidedirection: right;
+                                     easingin : easeOutQuart;
+                                     delayout: 0;
+                                     durationout : 1500;
+                                     slideoutdirection: left;
+                                     easingout : easeInOutQuint;
+                                    " />
+
+                            <h2 class="ls-s4 ls-head" style="
+                                    top: 70px;
+                                    left: 70px;
+                                    delayin : 400;
+                                    durationin : 1500;
+                                    slidedirection: right;
+                                    easingin : easeOutQuart;
+                                    delayout: 0;
+                                    durationout : 1500;
+                                    slideoutdirection: left;
+                                    easingout : easeInOutQuint;
+                                    ">Cadastre-se </h2>
+                            <p class="ls-s4 ls-sublabel" style="
+                                    top: 140px;
+                                    left: 70px;
+                                    delayin: 800;
+                                    durationin : 1500;
+                                    slidedirection: right;
+                                    easingin : easeOutQuart;
+                                    delayout: 0;
+                                    durationout : 1500;
+                                    slideoutdirection: left;
+                                    easingout : easeInOutQuint;
+                                    ">Valide usando seu e-mail da UFMG</p>
+                            <p class="ls-s4 ls-sublabel" style="
+                                    top: 165px;
+                                    left: 70px;
+                                    delayin: 1000;
+                                    durationin : 1500;
+                                    slidedirection: right;
+                                    easingin : easeOutQuart;
+                                    delayout: 0;
+                                    durationout : 1500;
+                                    slideoutdirection: left;
+                                    easingout : easeInOutQuint;
+                                    ">E compre aquele livro de Cálculo bem mais barato!</p>
+
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+            </section>
+            <!-- end: Slider -->
+        </div>
+
+        <div class="sub-inner">
+
+            <hr class="hidden-phone" />
+
+            <div class="row-fluid">
+                <section class="welcome pull-center">
+                    <h1>
+                        Entre no <span class="text-info">Liivro</span> e compre do pessoal da <strong>sua</strong> universidade!
+                    </h1>
+                    <h4>
+                        Só coisas de pessoas que importam, aquelas que você pode encontrar pessoalmente!
+                    </h4>
+                    <p>
+                        <a href="<?=$facebook_login_url;?>" class="btn btn-primary">ENTRAR COM FACEBOOK</a>
+                    </p>
+                </section>
+            </div>
+
+            <hr />
+             
+        </div>
+        </div>
+
+        </section>
+        <!-- end: Page section -->
+
+    </div>
+
+</div>
+<!-- end: Container -->
+
+<!-- start: Footer -->
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="span4">
+                <h3 class="widget-title">Endereço</h3>
+                <div class="widget-inner">
+                    <address>
+                        <ul class="icons">
+                            <li><i class="icon-map-marker"></i>UFMG/ICEx - Av. Antônio Carlos, 6627, Pampulha, Belo Horizonte - MG</li>
+                            <li><i class="icon-phone"></i>(31) 8235 5329</li>
+                            <li><i class="icon-envelope"></i><a href="mailto:#">jotavrj@gmail.com</a></li>
+                        </ul>
+                    </address>
+                    <p><a href="./contact.html" class="btn btn-primary">Contato</a></p>
+                </div>
+                <h3 class="widget-title">Follow Us</h3>
+                <div class="widget-inner">
+                    <ul class="social clearfix">
+                        <li><a href="#" data-original-title="Facebook" data-placement="top" rel="tooltip">
+                            <i class="icon-facebook-sign"></i>
+                        </a></li>
+                        <li><a href="#" data-original-title="Twitter" data-placement="top" rel="tooltip">
+                            <i class="icon-twitter-sign"></i>
+                        </a></li>
+                        <li><a href="#" data-original-title="Linkedin" data-placement="top" rel="tooltip">
+                            <i class="icon-linkedin-sign"></i>
+                        </a></li>
+                        <li><a href="#" data-original-title="Pinterest" data-placement="top" rel="tooltip">
+                            <i class="icon-pinterest-sign"></i>
+                        </a></li>
+                        <li><a href="#" data-original-title="Google+" data-placement="top" rel="tooltip">
+                            <i class="icon-google-plus-sign"></i>
+                        </a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="span4">
+                <h3 class="widget-title">Sobre</h3>
+                <div class="widget-inner">
+                    <p>O Liivro nasceu de uma Iniciação Científica sobre fraudes em transações online, e da forma como a relevância dos seus contatos
+                    diminui drasticamente este mal. Comprando de pessoas da própria universidade, onde as pessoas têm uma proximidade maior
+                    e a possibilidade de resgatar o produto pessoalmente, uma fraude é extremamente improvável.
+                    Levando isso para o contexto das redes sociais, se firma aí uma iniciativa super interessante.
+                    </p>
+                </div>
+                <h3 class="widget-title">Saiba mais</h3>
+                <div class="widget-inner">
+                    <p>Assine o newsletter!</p>
+                    <form />
+                        <div class="input-append">
+                            <input type="email" name="email" class="span3" placeholder="Email Address" />
+                            <button class="btn btn-primary">Assinar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="span4">
+                <h3 class="widget-title">Últimas do Blog</h3>
+                <div class="widget-inner">
+                    <ul class="icons lates-blog-post">
+                        <li><i class="icon-chevron-right"></i><a href="#">Lorem ipsum dolor sit amet</a><br />Aliquam nunc erat, suscipit molestie interdum eget, molestie vel eros.</li>
+                        <li><i class="icon-chevron-right"></i><a href="#">Quisque hendrerit nisl vitae magna </a><br />Aenean a sapien in magna luctus aliquet in eu purus. Sed venenatis luctus varius.</li>
+                        <li><i class="icon-chevron-right"></i><a href="#">Aenean gravida tincidunt velit nec congue </a><br />Sed a rhoncus mi. Morbi eu elit sapien, eu adipiscing nisl. Duis in turpis et mi molestie bibendum eget eget mauris.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- end: Footer -->
+
+<!-- start: Footer menu-->
+<section id="footer-menu">
+    <div class="container">
+        <div class="row">
+            <div class="span4">
+                <p class="copyright">Liivro - &copy; Copyright 2013</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- end: Footer menu-->
+
+</body>
+</html>
