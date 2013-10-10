5578d3fafa1435a35ef2fd7691afe7ef2cf81f46
Jota Junior <jotavrj@gmail.com>
2013-10-10 11:05:18 -0300

    Test session online (only possiblity because we are using memcached as session handler)

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -13,11 +13,11 @@ class IndexController extends \Phalcon\Mvc\Controller
 
 		if (!$auth) {
 			$facebook_login_url = $this->users->getFacebookLoginUrl();
-			
-			echo $this->view->render('landing/index', array(
-													   "facebook_login_url" => $facebook_login_url
-													  )
-								);
+		echo $facebook_login_url;	
+		//	echo $this->view->render('landing/index', array(
+		//											   "facebook_login_url" => $facebook_login_url
+		//											  )
+		//						);
 		} else {
 			echo "Ola, ".$this->session->get('name');
 						echo "<a href='/liivro/index/logout'>logout</a>";
@@ -25,6 +25,11 @@ class IndexController extends \Phalcon\Mvc\Controller
 		}
 	}
 	
+	public function testSession()
+	{
+		$this->session->set("whatever", 2);
+		var_dump($this->session->get("whatever"));
+	}
 	public function logoutAction()
 	{
 		$this->users->logout();
